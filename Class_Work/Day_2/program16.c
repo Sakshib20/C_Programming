acfcfba4df08f24aefa4c1da2cd7a0db4f8fbcaf
Sakshib20 <154331302+Sakshib20@users.noreply.github.com>
2025-10-23 13:42:10 +0530

    Add class work of Day2

---
+++ Class_Work/Day_2/program16.c
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Require Hear Files
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>               //for input output
+#include<stdbool.h>             //for bool datatype
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : CheckEvenOdd 
+//  Description:    It is used to check even or odd
+//  Input:          Integer
+//  Output:         Boolean
+//  Author:         Sakshi Pradeep Bhapkar
+//  Date:           10/10/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+bool CheckEvenOdd(int iNo)
+{
+    int iRem=0;
+    iRem=iNo%2;
+
+    if(iRem==0)
+    {
+        return true;}
+    else
+    {
+        return false;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function of the application
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue=0;
+    bool bRet=false;
+
+    printf("Enter number: \n");
+    scanf("%d",&iValue);
+
+    bRet=CheckEvenOdd(iValue);
+
+    if(bRet==true)
+    {
+        printf("%d is Even number\n",iValue);
+    }
+    else
+    {
+        printf("%d is Odd number\n",iValue);
+    }
+    return 0;
+}
