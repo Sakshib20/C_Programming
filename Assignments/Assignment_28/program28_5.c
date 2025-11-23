b130056e95675769d2803ccf3ed799731c332602
Sakshib20 <154331302+Sakshib20@users.noreply.github.com>
2025-11-23 21:22:53 +0530

    Add Assignment 28 solution (Q5)

---
+++ Assignments/Assignment_28/program28_5.c
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description : Displays the Pattern 
+//  Input  : Integer, Integer
+//  Output : Void
+//  Author : Sakshi Pradeep Bhapkar
+//  Date : 23/11/2025
+//
+///////////////////////////////////////////////////////////
+
+/*sample :
+Input : iRows=3  iCol=4
+
+Output : 1  2  3  4
+         5  6  7  8
+         9  10 11 12
+
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i=0, j=0, iCnt=0;
+
+    iCnt=1;
+    for(i=1;i<=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++,iCnt++)
+        {
+            printf("%d\t",iCnt);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1=0, iValue2=0;
+
+    printf("Enter the number of rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+    
+    return 0;
+}
